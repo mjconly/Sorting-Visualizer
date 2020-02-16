1b7d0e3b121000e65ef8b54aae0420942381713a
Mike <mjconly@gmail.com>
2020-02-16 17:38:26 -0600

    insertion sort implemented -- starting animation

--- src/sortingAlgorithms/insertionSort.js
+++ src/sortingAlgorithms/insertionSort.js
@@ -8,7 +8,23 @@ const insertionHelper = async (arr) => {
 }
 
 const insertionSort = async (arr) => {
-  return 1;
+  for (let i = 1; i < arr.length; i++){
+    const curr = arr[i];
+    const currVal = parseInt(arr[i].style.height.substring(0, arr[i].style.height.length -2))
+    let j = i;
+    let prevVal = parseInt(arr[j - 1].style.height.substring(0, arr[j - 1].style.height.length - 2));
+
+    while (j > 0 && currVal <= prevVal){
+      arr[j].style.height = `${prevVal}px`;
+      j -= 1;
+      if (j > 0){
+        prevVal = parseInt(arr[j - 1].style.height.substring(0, arr[j - 1].style.height.length - 2));
+      }
+    }
+
+    arr[j].style.height = `${currVal}px`;
+  }
+
 }
 
 export { insertionHelper }
