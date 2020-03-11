9ac964a50064d5e632037b12f492e9b9cec1cea6
Mike <mjconly@gmail.com>
2020-03-11 16:58:51 -0600

    try react helmet for meta tags

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import {Helmet} from "react-helmet";
 import './App.css';
 import ArrayBarContainer from "./Components/ArrayBarContainer"
 
 function App() {
   return (
     <div className="App col-sm-10 m-auto">
+      <helmet>
+        <meta
+          name="description"
+          content="Visualize Popular Sorting Algorithms"
+        />
+        <meta name="image" content="./sortingImage.png">
+      </helmet>
       <ArrayBarContainer></ArrayBarContainer>
     </div>
   );
