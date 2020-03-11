b15e84019fb648dfec1ae70876c275aa02978136
Mike <mjconly@gmail.com>
2020-03-11 17:36:27 -0600

    trying og tags again

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React from 'react';
-import {Helmet} from "react-helmet";
 import './App.css';
 import ArrayBarContainer from "./Components/ArrayBarContainer"
 
 function App() {
   return (
     <div className="App col-sm-10 m-auto">
-      <Helmet>
-        <meta
-          name="og:description"
-          content="Visualize Popular Sorting Algorithms"
-        />
-      <meta name="og:image" content="./sortingImage.png"/>
-      </Helmet>
       <ArrayBarContainer></ArrayBarContainer>
     </div>
   );
